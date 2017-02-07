90ffa2a1fdeec27703573fff30fafd16f7ab3002
Mariano Merchante <merchante.mariano@gmail.com>
2017-02-07 10:44:13 -0500

    + A more formal LSystem definition

--- src/lsystem.js
+++ src/lsystem.js
@@ -5,44 +5,265 @@ function Rule(prob, str) {
 	this.successorString = str; // The string that will replace the char that maps to this Rule
 }
 
-// TODO: Implement a linked list class and its requisite functions
-// as described in the homework writeup
-
-
-// TODO: Turn the string into linked list 
-export function stringToLinkedList(input_string) {
-	// ex. assuming input_string = "F+X"
-	// you should return a linked list where the head is 
-	// at Node('F') and the tail is at Node('X')
-	var ll = new LinkedList();
-	return ll;
+// An instruction is essentially a symbol with logic, context, stack and (TODO) parameters
+class LInstruction
+{
+	symbol() { return "A"; }
+	evaluate(context, stack) { return context; }
 }
 
-// TODO: Return a string form of the LinkedList
-export function linkedListToString(linkedList) {
-	// ex. Node1("F")->Node2("X") should be "FX"
-	var result = "";
-	return result;
+class ForwardInstruction extends LInstruction 
+{  
+	symbol() { return "F"; }
+
+	evaluate(context, stack) {
+		return context;
+	}
 }
 
-// TODO: Given the node to be replaced, 
-// insert a sub-linked-list that represents replacementString
-function replaceNode(linkedList, node, replacementString) {
+// Dummy instructions can be anything, they are used for replacement
+class DummyInstruction extends LInstruction 
+{
+	constructor(symbol) { super(); this.dummySymbol = symbol; }
+
+	symbol() { return this.dummySymbol; }
+
+	evaluate(context, stack) {
+		return context;
+	}
 }
 
-export default function Lsystem(axiom, grammar, iterations) {
-	// default LSystem
-	this.axiom = "FX";
+class PushInstruction extends LInstruction 
+{  
+	symbol() { return "["; }
+
+	evaluate(context, stack) {
+	stack.push(context);
+		return context;
+	}
+}
+
+class PullInstruction extends LInstruction
+{
+	symbol() { return "]"; }
+
+	evaluate(context, stack) {
+		return stack.pop(context);
+	}
+}
+
+class RotatePositiveInstruction extends LInstruction
+{
+	symbol() { return "+"; }
+
+	evaluate(context, stack) {
+		return context;
+	}
+}
+
+
+class RotateNegativeInstruction extends LInstruction
+{
+	symbol() { return "-"; }
+
+	evaluate(context, stack) {
+		return context;
+	}
+}
+
+// A grammar chain is a doubly linked list of instructions
+// that can be modified by given rules
+class LInstructionChain 
+{
+  constructor()
+  {
+    this.root = null;
+    this.last = null;
+  }
+
+  push(value) 
+  {
+    if(this.root == null)
+    {
+      this.root = { prev: null, next: null, value: value, new : false};
+      this.last = this.root;
+    }
+    else if(this.last != null)
+    {
+      var node = { prev: this.last, next: null, value: value, new : true};
+      this.last.next = node;
+      this.last = node;
+    }
+
+    return this.last;
+  }
+
+  // Evaluates a chain of instructions, both with a context and a stack
+  evaluate()
+  {    
+    var contextStack = [];
+    var context = { position: 0, angle : 0 };
+
+    this.evaluateInternal(function(node) {
+      context = node.value.evaluate(context, contextStack);
+    });
+  }
+
+  evaluateInternal(evaluateFunc)
+  {
+    this.iterate(null, null, evaluateFunc);
+  }
+
+  // General purpose iteration function
+  iterate(condition, returnFunc, evaluateFunc = null)
+  {
+    var node = this.root;
+
+    while(node != null) {
+
+      if(evaluateFunc != null)
+        evaluateFunc(node);
+
+      if(returnFunc != null && condition != null && condition(node))
+        return returnFunc(node);
+
+      node = node.next;
+    }
+
+    return null;
+  }
+
+  toString() 
+  {
+    var result = "";
+    this.evaluateInternal(function(node) { result += node.value.symbol(); } );
+    return result;
+  }
+
+  findAll(value) 
+  {
+    var nodes = [];
+    this.iterate(null, null, function(node) { if(node.value == value) nodes.push(node); });
+    return nodes;
+  }
+
+  find(value) 
+  {
+    return this.iterate(function(node){return node.value == value;}, function(node) { return node } );
+  }
+
+  // Because we're expanding in-place, we must be careful not to 
+  // expand recently added nodes that come from a previous replacement
+  // in the same expansion cycle.
+  expand(rules, clean)
+  {
+    if(clean)
+      this.iterate(null, null, function(node){node.new = false;});
+
+    for(var r = 0; r < rules.length; r++)
+    {
+      var nodes = this.findAll(rules[r].predecessor());
+
+      for(var n = 0; n < rules.length; n++)
+        if(nodes[n].new == false)
+          this.replace(nodes[n], rules[r].successor())
+    }
+
+    this.iterate(null, null, function(node){node.new = false;});
+  }
+
+  // Now it only replaces one symbol. TODO context aware rules
+  replaceSymbol(v, values)
+  {
+    this.replace(this.find(v), values);
+  }
+
+  replace(node, values) 
+  {
+    if(node == null)
+      return;
+
+    var prevNode = node.prev;
+    this.last = prevNode;
+
+    if(this.root == node)
+      this.root = this.last;
+
+    for(var i = 0; i < values.length; i++)
+      this.push(values[i]);
+
+    // Reconnect the chain, while ignoring the replaced node
+    if(this.last != null)
+    {
+      this.last.next = node.next;
+
+      if(node.next != null)
+        node.next.prev = this.last;
+
+      // Make sure we update the last node
+      while(this.last.next != null)
+        this.last = this.last.next;
+    }
+  }
+}
+
+class LRule 
+{
+	probability() { return 1.0; }	
+
+	// The symbol to replace
+	predecessor() { return new LInstruction(); }
+
+	// The list of symbols that are placed instead
+	successor() { return [new LInstruction(), new LInstruction()]; }
+}
+
+export default function LSystem(axiom, grammar, iterations) 
+{
+	this.registerInstruction = function(instruction)
+	{
+		this.instructionMap[instruction.symbol()] = instruction;
+	}
+
+	this.getInstruction = function(symbol) 
+	{
+		if(!(symbol in this.instructionMap))
+			console.error("Symbol " + symbol + " not present in instruction map!");
+
+		return this.instructionMap[symbol];
+	}
+
+	this.parseAxiom = function(axiomSymbols) 
+	{
+		this.chain = new LInstructionChain();
+
+		for(var i = 0; i < axiomSymbols.length; i++)
+			this.chain.push(this.getInstruction(axiomSymbols[i]));
+	}
+
+	this.updateAxiom = function(axiom) 
+	{
+		this.axiom = axiom;
+		this.parseAxiom(axiom);
+	}
+
 	this.grammar = {};
 	this.grammar['X'] = [
 		new Rule(1.0, '[-FX][+FX]')
 	];
-	this.iterations = 0; 
+	this.iterations = iterations;
+	this.instructionMap = {};
+	this.chain = new LInstructionChain();
+
+	// Register common instructions
+	this.registerInstruction(new PushInstruction());
+	this.registerInstruction(new PullInstruction());
+	this.registerInstruction(new ForwardInstruction());
+	this.registerInstruction(new DummyInstruction("X"));
+	this.registerInstruction(new RotateNegativeInstruction());
+	this.registerInstruction(new RotatePositiveInstruction());
 	
-	// Set up the axiom string
-	if (typeof axiom !== "undefined") {
-		this.axiom = axiom;
-	}
+	this.updateAxiom(axiom);	
 
 	// Set up the grammar as a dictionary that 
 	// maps a single character (symbol) to a Rule.
@@ -50,21 +271,6 @@ export default function Lsystem(axiom, grammar, iterations) {
 		this.grammar = Object.assign({}, grammar);
 	}
 	
-	// Set up iterations (the number of times you 
-	// should expand the axiom in DoIterations)
-	if (typeof iterations !== "undefined") {
-		this.iterations = iterations;
-	}
-
-	// A function to alter the axiom string stored 
-	// in the L-system
-	this.updateAxiom = function(axiom) {
-		// Setup axiom
-		if (typeof axiom !== "undefined") {
-			this.axiom = axiom;
-		}
-	}
-
 	// TODO
 	// This function returns a linked list that is the result 
 	// of expanding the L-system's axiom n times.
--- src/main.js
+++ src/main.js
@@ -1,167 +1,11 @@
 
 const THREE = require('three'); // older modules are imported like this. You shouldn't have to worry about this much
 import Framework from './framework'
-import Lsystem, {LinkedListToString} from './lsystem.js'
+import LSystem from './lsystem.js'
 import Turtle from './turtle.js'
 
 var turtle;
 
-class LInstruction
-{
-  constructor()
-  {    
-  }
-
-  symbol()
-  {
-    return "A";
-  }
-
-  evaluate(context, stack)
-  {
-    // Do stuff
-  }
-}
-
-// var PushInstruction = new LInstruction
-
-// A grammar chain is a doubly linked list of instructions
-// that can be modified by given rules
-class GrammarChain 
-{
-  constructor()
-  {
-    this.root = null;
-    this.last = null;
-  }
-
-  push(value) 
-  {
-    if(this.root == null)
-    {
-      this.root = { prev: null, next: null, value: value, new : false};
-      this.last = this.root;
-    }
-    else if(this.last != null)
-    {
-      var node = { prev: this.last, next: null, value: value, new : true};
-      this.last.next = node;
-      this.last = node;
-    }
-
-    return this.last;
-  }
-
-  // Evaluates a chain of instructions, both with a context and a stack
-  evaluate()
-  {    
-    contextStack = [];
-    context = { position: 0, angle : 0 };
-
-    this.evaluateInternal(function(node){
-      context = node.value.evaluate(context, contextStack);
-    });
-  }
-
-  evaluateInternal(evaluateFunc)
-  {
-    this.iterate(null, null, evaluateFunc);
-  }
-
-  // General purpose iteration function
-  iterate(condition, returnFunc, evaluateFunc = null)
-  {
-    var node = this.root;
-
-    while(node != null) {
-
-      if(evaluateFunc != null)
-        evaluateFunc(node);
-
-      if(returnFunc != null && condition != null && condition(node))
-        return returnFunc(node);
-
-      node = node.next;
-    }
-
-    return null;
-  }
-
-  toString() 
-  {
-    var result = "";
-    this.evaluateInternal(function(node) { result += node.value; } );
-    return result;
-  }
-
-  findAll(value) 
-  {
-    var nodes = [];
-    this.iterate(null, null, function(node) { if(node.value == value) nodes.push(node); });
-    return nodes;
-  }
-
-  find(value) 
-  {
-    return this.iterate(function(node){return node.value == value;}, function(node) { return node } );
-  }
-
-  // Because we're replacing in-place, we must be careful not to 
-  // replace recently added nodes that come from a previous replacement
-  // in the same expansion cycle.
-  expand(rules, clean)
-  {
-    if(clean)
-      this.iterate(null, null, function(node){node.new = false;});
-
-    for(var r = 0; r < rules.length; r++)
-    {
-      var nodes = this.findAll(rules[r].symbol);
-
-      for(var n = 0; n < rules.length; n++)
-        if(nodes[n].new == false)
-          this.replace(nodes[n], rules[r].resultSymbols)
-    }
-
-    this.iterate(null, null, function(node){node.new = false;});
-  }
-
-  // Now it only replaces one symbol. TODO context aware rules
-  replaceSymbol(v, values)
-  {
-    this.replace(this.find(v), values);
-  }
-
-  replace(node, values) 
-  {
-    if(node == null)
-      return;
-
-    var prevNode = node.prev;
-    this.last = prevNode;
-
-    if(this.root == node)
-      this.root = this.last;
-
-    for(var i = 0; i < values.length; i++)
-      this.push(values[i]);
-
-    // Reconnect the chain, while ignoring the replaced node
-    if(this.last != null)
-    {
-      this.last.next = node.next;
-
-      if(node.next != null)
-        node.next.prev = this.last;
-
-      // Make sure we update the last node
-      while(this.last.next != null)
-        this.last = this.last.next;
-    }
-  }
-}
-
-// called after the scene loads
 function onLoad(framework) {
   var scene = framework.scene;
   var camera = framework.camera;
@@ -180,41 +24,46 @@ function onLoad(framework) {
   camera.position.set(1, 1, 2);
   camera.lookAt(new THREE.Vector3(0,0,0));
 
-  // initialize LSystem and a Turtle to draw
-  var lsys = new Lsystem();
-  turtle = new Turtle(scene);
+  // // initialize LSystem and a Turtle to draw
+  // var lsys = new Lsystem();
+  // turtle = new Turtle(scene);
 
-  gui.add(camera, 'fov', 0, 180).onChange(function(newVal) {
-    camera.updateProjectionMatrix();
-  });
+  // gui.add(camera, 'fov', 0, 180).onChange(function(newVal) {
+  //   camera.updateProjectionMatrix();
+  // });
 
-  gui.add(lsys, 'axiom').onChange(function(newVal) {
-    lsys.UpdateAxiom(newVal);
-    doLsystem(lsys, lsys.iterations, turtle);
-  });
+  // gui.add(lsys, 'axiom').onChange(function(newVal) {
+  //   lsys.UpdateAxiom(newVal);
+  //   doLsystem(lsys, lsys.iterations, turtle);
+  // });
 
-  gui.add(lsys, 'iterations', 0, 12).step(1).onChange(function(newVal) {
-    clearScene(turtle);
-    doLsystem(lsys, newVal, turtle);
-  });
+  // gui.add(lsys, 'iterations', 0, 12).step(1).onChange(function(newVal) {
+  //   clearScene(turtle);
+  //   doLsystem(lsys, newVal, turtle);
+  // });
+
+  var lSystem = new LSystem("][][", "", 1);
+
+  console.log(lSystem.axiom);
+  console.log(lSystem.chain);
+  console.log(lSystem.chain.toString());
+  console.log(lSystem.instructionMap);
+
+  // var chain = new LInstructionChain();
+  // chain.push(new PushInstruction());
+  // chain.push(new PushInstruction());
+  // chain.push(new PullInstruction());
+  // chain.push(new PullInstruction());
+
+  // // chain.replaceSymbol("B", ["Q", "Q", "Q"]);
 
 
-  var list = new GrammarChain();
-  list.push("A");
-  list.push("B");
-  list.push("C");
-  list.push("D");
-  list.push("E");
-  list.push("F");
+  // chain.evaluate();
+  // // chain.push("H");
 
-  list.replaceSymbol("B", ["Q", "Q", "Q"]);
+  // console.log(chain.root);
 
-
-  list.push("H");
-
-  console.log(list.root);
-
-  console.log(list.toString());
+  // console.log(chain.toString());
 }
 
 // clears the scene by removing all geometries added by turtle.js
