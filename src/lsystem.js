97993dee1f76af55d149af84d6f92609d36c01f4
Mohamed Soudy <msoudy7@gmail.com>
2017-02-07 18:02:02 -0500

    Compleled Project 3

--- src/lsystem.js
+++ src/lsystem.js
@@ -7,6 +7,174 @@ function Rule(prob, str) {
 
 // TODO: Implement a linked list class and its requisite functions
 // as described in the homework writeup
+function Node(grammarSymbol) {
+    this.symbol = grammarSymbol;
+	this.param = [];
+	this.cond = null;
+	/*this.cond = function (s , min) {
+		return s >= min;
+	};*/
+    this.previous = null;
+    this.next = null;
+}
+ 
+function LinkedList() {
+    this.head = null;
+    this.tail = null;
+	this.rotation = 30;
+}
+
+LinkedList.prototype.setSym = function (nodeA, nodeB) {
+	if (nodeA)
+		nodeA.next = nodeB;
+	if (nodeB)
+		nodeB.prev = nodeA;
+}
+
+
+LinkedList.prototype.printParameter = function () {
+	
+	var temp = this.head;
+	var str = "";
+	while (temp) {
+		
+		str += (temp.symbol);
+		if (temp.symbol == 'A') {
+			
+			str += "(" + temp.param[0] + "," + temp.param[1] + ")";
+		}
+		
+		else if (temp.symbol != '[' && temp.symbol != ']') {
+
+			str += "(" + temp.param[0] + ")";
+		}
+		
+		
+		//str += ("-->");	
+		temp = temp.next;
+	}
+	
+	console.log(str);
+
+}
+
+LinkedList.prototype.print = function () {
+	
+	var temp = this.head;
+	var str = "";
+	while (temp) {
+		
+		str += (temp.symbol);
+		//str += ("-->");	
+		temp = temp.next;
+	}
+	
+	console.log(str);
+
+}
+
+LinkedList.prototype.printReversed = function () {
+	
+	var temp = this.tail;
+	var str = "";
+	while (temp) {
+		
+		str += (temp.symbol);
+		//sstr += ("-->");	
+		temp = temp.prev;
+	}
+	
+	console.log(str);
+
+}
+
+// replaceNode(linkedList, node, replacementString)
+LinkedList.prototype.expand = function (node, grammar) {
+	
+	var rules = grammar[node.symbol];
+	
+	if (rules) {
+		
+		var r = Math.random();
+		var selectedRule = rules[0];
+		
+		
+		for (var i = 0; i < rules.length; i++) {
+
+			if (rules[i].probability >= r) {
+				selectedRule = rules[i];
+			}
+		}
+		
+		
+		replaceNode(this, node, selectedRule.successorString);
+	}
+	
+}
+
+
+LinkedList.prototype.expandParameter = function (node) {
+	
+	
+	if (node.symbol != "A")
+		return;
+	
+	var r1 = 0.8;
+	var r2 = 0.8;
+	var alpha1 = 30.0;
+	var alpha2 = -30.0;
+	var phi1 = 137.0;
+	var phi2 = 137.0;
+	var q = 0.5;
+	var e = 0.5;
+	var min = 0.0;
+	var n = 10;
+	var s = node.param[0];
+	var w = node.param[1];
+	
+	var str = "!";
+	str += "(" + w + ")"; //w
+	str += "F(" + s + ")"; //s
+	str += "[+(" + alpha1 + ")";
+	str += "/(" + phi1 + ")";	
+	str += "A(" + s * r1 + "," + w * Math.pow(q,e) + ")]";	
+
+	str += "[+(" + alpha2 + ")";
+	str += "/(" + phi2 + ")";	
+	str += "A(" + s * r2 + "," + w * Math.pow(1.0-q,e) + ")]";	
+	//console.log(str);
+	
+	replaceNode(this, node, str);
+}
+
+LinkedList.prototype.addNode = function(node) {
+ 
+    if (this.head != null) {
+        this.tail.next = node;
+        node.prev = this.tail;
+        this.tail = node;
+    } else {
+        this.head = node;
+        this.tail = node;
+    }
+  
+    return node;
+};
+ 
+LinkedList.prototype.add = function(value) {
+    var node = new Node(value);
+ 
+    if (this.head != null) {
+        this.tail.next = node;
+        node.prev = this.tail;
+        this.tail = node;
+    } else {
+        this.head = node;
+        this.tail = node;
+    }
+  
+    return node;
+};
 
 // TODO: Turn the string into linked list 
 export function stringToLinkedList(input_string) {
@@ -14,6 +182,66 @@ export function stringToLinkedList(input_string) {
 	// you should return a linked list where the head is 
 	// at Node('F') and the tail is at Node('X')
 	var ll = new LinkedList();
+	
+	for (var i = 0, len = input_string.length; i < len; i++) {
+  		ll.add(input_string[i]);
+	}
+	
+	return ll;
+}
+
+export function stringParameterToLinkedList(input_string) {
+	// ex. assuming input_string = "F+X"
+	// you should return a linked list where the head is 
+	// at Node('F') and the tail is at Node('X')	
+	
+	//console.log(input_string);
+	var str = input_string.replace(/ *\([^)]*\) */g, ""); //strip out params
+	var params = input_string.match(/ *\([^)]*\) */g); //save params	
+	//var str2 = "!(w)F(s)[+(30)/(137)A(12,23)][+(-30)/(137)A(22,110)]";
+	//var myArray = input_string.match(/ *\([^)]*\) */g);
+	//console.log(myArray);
+	
+	var ll = new LinkedList();
+	var offset = 0;
+	
+	for (var i = 0; i < str.length; i++)
+	{
+		var node = new Node(str[i]);
+		
+		if (str[i] == '[' || str[i] == ']') {
+			
+			offset++;
+			ll.addNode(node);
+			
+		} 
+		else if (str[i] != 'A') {
+			
+			var re = /\(([+-]?\d+\.?\d*)\).*/; 
+			var symbolParams = params[i-offset].match(re); //get params
+			
+			for (var j = 0; j < symbolParams.length-1; j++) {
+				node.param[j] = symbolParams[j+1]
+			}
+			
+			ll.addNode(node);		
+		} 
+		else {
+			
+			var re = /\(([+-]?\d+\.?\d*),([+-]?\d+\.?\d*)\)/; 
+			var symbolParams = params[i-offset].match(re); //get params
+			
+			for (var j = 0; j < symbolParams.length-1; j++) {
+				node.param[j] = symbolParams[j+1]
+			}
+			node.cond = 's >= 0';
+			
+			ll.addNode(node);			
+		}
+	}
+	
+	//console.log(ll);
+	
 	return ll;
 }
 
@@ -21,21 +249,77 @@ export function stringToLinkedList(input_string) {
 export function linkedListToString(linkedList) {
 	// ex. Node1("F")->Node2("X") should be "FX"
 	var result = "";
+	
+	var temp = linkedList.head;
+	
+	while(temp.next) {
+		
+		result+= temp.symbol;
+		temp = temp.next;
+	}
+	
 	return result;
 }
 
 // TODO: Given the node to be replaced, 
 // insert a sub-linked-list that represents replacementString
 function replaceNode(linkedList, node, replacementString) {
+	
+	if (replacementString == "")
+		return;
+	
+	var subLinkedList = stringToLinkedList(replacementString);
+	//var subLinkedList = stringParameterToLinkedList(replacementString);
+
+	//if node is head
+	if (node.prev == null) {
+		
+		linkedList.setSym(subLinkedList.tail, linkedList.head.next);		
+		linkedList.head = subLinkedList.head;
+		
+	} else if (node.next == null) { //node is tail
+				
+		linkedList.setSym(linkedList.tail.prev, subLinkedList.head);		
+		linkedList.tail = subLinkedList.tail;
+		
+	} else { //node is in middle of list
+		
+		linkedList.setSym(node.prev, subLinkedList.head);
+		linkedList.setSym(subLinkedList.tail, node.next);		
+	}
+	
 }
 
 export default function Lsystem(axiom, grammar, iterations) {
 	// default LSystem
-	this.axiom = "FX";
+	//this.axiom = "A(100.0,30.0)";
+	this.rotation = 30.0;
+	this.axiom = "X";
 	this.grammar = {};
 	this.grammar['X'] = [
-		new Rule(1.0, '[-FX][+FX]')
+		new Rule(1.0, 'F[+XS]F[-/XS]+X')
+		//new Rule(0.5, 'F[-XS]F[+&XS]+X')
 	];
+	this.grammar['F'] = [
+		new Rule(1.0, 'F')
+	];
+	/*this.grammar['X'] = [
+		new Rule(1.0, '[-FX][+FX]')
+	];*/
+	/*this.grammar['X'] = [
+		new Rule(1.0, '+v[GY]v[GY]v[GY]'),
+		//new Rule(1.0, '-v[+GY]v[+GY]v[+GY]')
+	];
+	
+	this.grammar['Y'] = [
+		new Rule(1.0, 'v[GY]v[HZ]')
+	];
+	
+	this.grammar['Z'] = [
+		new Rule(1.0, '[+wJZ][+wwJZ][+wwwJZ]')
+	];*/
+	
+	
 	this.iterations = 0; 
 	
 	// Set up the axiom string
@@ -63,14 +347,44 @@ export default function Lsystem(axiom, grammar, iterations) {
 			this.axiom = axiom;
 		}
 	}
-
+	
+	this.updateRotation = function(rotation) {
+		// Setup axiom
+		if (typeof rotation !== "undefined") {
+			this.rotation = rotation;
+		}
+	}
+	
 	// TODO
 	// This function returns a linked list that is the result 
 	// of expanding the L-system's axiom n times.
 	// The implementation we have provided you just returns a linked
 	// list of the axiom.
 	this.doIterations = function(n) {	
-		var lSystemLL = StringToLinkedList(this.axiom);
+		var lSystemLL = stringToLinkedList(this.axiom);
+		lSystemLL.rotation = this.rotation;
+		//var lSystemLL = stringParameterToLinkedList(this.axiom);
+
+		var temp = n;
+		while (n > 0) {
+			
+			var temp = lSystemLL.head;
+	
+			while(temp) {
+
+				var next = temp.next;
+				
+				var node = lSystemLL.expand(temp, this.grammar);
+				//var node = lSystemLL.expandParameter(temp, this.grammar);
+
+				
+				temp = next;
+			}			
+			n--;	
+		}
+		//lSystemLL.printParameter();
+		//lSystemLL.print();
+	
 		return lSystemLL;
 	}
 }
--- src/turtle.js
+++ src/turtle.js
@@ -10,24 +10,101 @@ var TurtleState = function(pos, dir) {
         dir: new THREE.Vector3(dir.x, dir.y, dir.z)
     }
 }
+
   
 export default class Turtle {
-    
+    	
     constructor(scene, grammar) {
         this.state = new TurtleState(new THREE.Vector3(0,0,0), new THREE.Vector3(0,1,0));
         this.scene = scene;
+		this.states = [];
+		//this.lineWidth = 3.0;
 
         // TODO: Start by adding rules for '[' and ']' then more!
         // Make sure to implement the functions for the new rules inside Turtle
-        if (typeof grammar === "undefined") {
+		if (typeof grammar === "undefined") {
+			
+			this.renderGrammar = function(node) {
+				switch(node.symbol) {
+					case '!':
+						this.lineWidth = node.param[0];
+						break;
+					case '+':
+						this.rotateTurtle(node.param[0], 0, 0);
+						//this.rotateTurtle(0, -node.param[0], 0);
+						break;
+					case '/':
+						//this.rotateTurtle(0, 0, -node.param[0]);
+						var a = Math.random();
+						this.rotateTurtle(0, 0, node.param[0]);
+						break;					
+					case '&':
+						var a = Math.random();
+						this.rotateTurtle(0, 0, -node.param[0]);
+						//this.rotateTurtle(0, 0, -node.param[0]);
+						break;
+					case '-':
+						this.rotateTurtle(-node.param[0], 0, 0);
+						break;
+					case 'v':
+						this.rotateTurtle(0, 120, 0);
+						break;
+					case 'w':
+						this.rotateTurtle(0, 60, 0);
+						break;
+					case 'F':
+						//for (var i = 0; i < 10; i++)
+							this.makeCylinder(2, 0.1)
+						//this.makeCylinder(node.param[0], this.lineWidth);
+						break;
+					case 'G':
+						for (var i = 0; i < 8; i++)
+							this.makeCylinder(2, 2.0)						//this.makeCylinder(node.param[0], this.lineWidth);
+						break;
+					case 'H':
+						for (var i = 0; i < 6; i++)
+							this.makeCylinder(2, 1.0)						//this.makeCylinder(node.param[0], this.lineWidth);
+						break;
+					case 'J':
+						for (var i = 0; i < 4; i++)
+							this.makeCylinder(2, 0.5)						//this.makeCylinder(node.param[0], this.lineWidth);
+					case 'S':
+							this.makeSphere(2, 0.3)
+						break;
+					case '[':
+						this.pushState();
+						break;
+					case ']':
+						this.popState();
+						break;
+					default:
+						break;
+				}
+
+			}
+			
+			
+		} else {
+            this.renderGrammar = grammar;
+        }
+		
+		
+		
+        /*if (typeof grammar === "undefined") {
             this.renderGrammar = {
                 '+' : this.rotateTurtle.bind(this, 30, 0, 0),
                 '-' : this.rotateTurtle.bind(this, -30, 0, 0),
-                'F' : this.makeCylinder.bind(this, 2, 0.1)
+				'v' : this.rotateTurtle.bind(this, 0, 120, 0),
+				'w' : this.rotateTurtle.bind(this, 0, 60, 0),
+                'F' : this.makeCylinder.bind(this, 2, 0.1),
+				//'G' : this.makeCylinder.bind(this, 1, 0.3),
+               // 'H' : this.makeCylinder.bind(this, 1, 0.1),
+				'[' : this.pushState.bind(this),
+				']' : this.popState.bind(this)
             };
         } else {
             this.renderGrammar = grammar;
-        }
+        }*/
     }
 
     // Resets the turtle's position to the origin
@@ -42,6 +119,14 @@ export default class Turtle {
         console.log(this.state.pos)
         console.log(this.state.dir)
     }
+	
+	pushState() {
+		this.states.push(new TurtleState(this.state.pos, this.state.dir));
+	}
+	
+	popState() {
+		this.state = this.states.pop();
+	}
 
     // Rotate the turtle's _dir_ vector by each of the 
     // Euler angles indicated by the input.
@@ -70,7 +155,7 @@ export default class Turtle {
     // Moves turtle pos ahead to end of the new cylinder
     makeCylinder(len, width) {
         var geometry = new THREE.CylinderGeometry(width, width, len);
-        var material = new THREE.MeshBasicMaterial( {color: 0x00cccc} );
+        var material = new THREE.MeshBasicMaterial( {color: 0xFF69B4} );
         var cylinder = new THREE.Mesh( geometry, material );
         this.scene.add( cylinder );
 
@@ -92,20 +177,48 @@ export default class Turtle {
         this.moveForward(len/2);
     };
     
+	
+	makeSphere(len, width) {
+        var geometry = new THREE.SphereGeometry(width, width, len);
+        var material = new THREE.MeshBasicMaterial( {color: 0xffffff} );
+        var cylinder = new THREE.Mesh( geometry, material );
+        this.scene.add( cylinder );
+
+        //Orient the cylinder to the turtle's current direction
+        var quat = new THREE.Quaternion();
+        quat.setFromUnitVectors(new THREE.Vector3(0,1,0), this.state.dir);
+        var mat4 = new THREE.Matrix4();
+        mat4.makeRotationFromQuaternion(quat);
+        cylinder.applyMatrix(mat4);
+
+
+        //Move the cylinder so its base rests at the turtle's current position
+        var mat5 = new THREE.Matrix4();
+        var trans = this.state.pos.add(this.state.dir.multiplyScalar(0.5 * len));
+        mat5.makeTranslation(trans.x, trans.y, trans.z);
+        cylinder.applyMatrix(mat5);
+
+        //Scoot the turtle forward by len units
+        this.moveForward(len/2);
+    };
+	
     // Call the function to which the input symbol is bound.
     // Look in the Turtle's constructor for examples of how to bind 
     // functions to grammar symbols.
     renderSymbol(symbolNode) {
-        var func = this.renderGrammar[symbolNode.character];
-        if (func) {
-            func();
-        }
+       // var func = this.renderGrammar[symbolNode.symbol];
+		var func = this.renderGrammar(symbolNode);
+		if (func) {
+			func();
+		}
+
     };
 
     // Invoke renderSymbol for every node in a linked list of grammar symbols.
     renderSymbols(linkedList) {
         var currentNode;
         for(currentNode = linkedList.head; currentNode != null; currentNode = currentNode.next) {
+			currentNode.param[0] = linkedList.rotation;
             this.renderSymbol(currentNode);
         }
     }
